f9b677b90d034f546937809ef4337879cd23301c
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2018-06-28 16:45:59 -0700

    Revert "Trimmed the branch to only the fastrtps_cmake_module sub directory"
    This reverts commit d1521836f0b4468eaae10ea34fe49c055287af09.

---
+++ rmw_fastrtps_cpp/src/ros_message_serialization.cpp
@@ -0,0 +1,76 @@
+// Copyright 2016 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "fastcdr/Cdr.h"
+#include "fastcdr/FastBuffer.h"
+
+#include "./type_support_common.hpp"
+
+size_t
+_get_serialized_size(
+  const void * ros_message,
+  void * untyped_typesupport,
+  const char * typesupport_identifier)
+{
+  if (using_introspection_c_typesupport(typesupport_identifier)) {
+    auto typed_typesupport = static_cast<MessageTypeSupport_c *>(untyped_typesupport);
+    return typed_typesupport->getEstimatedSerializedSize(ros_message);
+  } else if (using_introspection_cpp_typesupport(typesupport_identifier)) {
+    auto typed_typesupport = static_cast<MessageTypeSupport_cpp *>(untyped_typesupport);
+    return typed_typesupport->getEstimatedSerializedSize(ros_message);
+  }
+  RMW_SET_ERROR_MSG("Unknown typesupport identifier");
+  return 0;
+}
+
+bool
+_serialize_ros_message(
+  const void * ros_message,
+  eprosima::fastcdr::FastBuffer & buffer,
+  eprosima::fastcdr::Cdr & ser,
+  void * untyped_typesupport,
+  const char * typesupport_identifier)
+{
+  if (using_introspection_c_typesupport(typesupport_identifier)) {
+    auto typed_typesupport = static_cast<MessageTypeSupport_c *>(untyped_typesupport);
+    size_t estimated_size = typed_typesupport->getEstimatedSerializedSize(ros_message);
+    buffer.reserve(estimated_size);
+    return typed_typesupport->serializeROSmessage(ros_message, ser);
+  } else if (using_introspection_cpp_typesupport(typesupport_identifier)) {
+    auto typed_typesupport = static_cast<MessageTypeSupport_cpp *>(untyped_typesupport);
+    size_t estimated_size = typed_typesupport->getEstimatedSerializedSize(ros_message);
+    buffer.reserve(estimated_size);
+    return typed_typesupport->serializeROSmessage(ros_message, ser);
+  }
+  RMW_SET_ERROR_MSG("Unknown typesupport identifier");
+  return false;
+}
+
+bool
+_deserialize_ros_message(
+  eprosima::fastcdr::Cdr & deser,
+  void * ros_message,
+  void * untyped_typesupport,
+  const char * typesupport_identifier)
+{
+  if (using_introspection_c_typesupport(typesupport_identifier)) {
+    auto typed_typesupport = static_cast<TypeSupport_c *>(untyped_typesupport);
+    return typed_typesupport->deserializeROSmessage(deser, ros_message);
+  } else if (using_introspection_cpp_typesupport(typesupport_identifier)) {
+    auto typed_typesupport = static_cast<TypeSupport_cpp *>(untyped_typesupport);
+    return typed_typesupport->deserializeROSmessage(deser, ros_message);
+  }
+  RMW_SET_ERROR_MSG("Unknown typesupport identifier");
+  return false;
+}
