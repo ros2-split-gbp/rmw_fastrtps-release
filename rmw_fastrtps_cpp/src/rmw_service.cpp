db9940ba84da68bbd15892dc9a113c0c9b611fdb
Jacob Perron <jacob@openrobotics.org>
2019-05-08 16:23:59 -0700

    Imported upstream version '0.7.1' of 'upstream'

--- rmw_fastrtps_cpp/src/rmw_service.cpp
+++ rmw_fastrtps_cpp/src/rmw_service.cpp
@@ -30,15 +30,15 @@
 #include "rmw/allocators.h"
 #include "rmw/rmw.h"
 
-#include "rmw_fastrtps_shared_cpp/rmw_common.hpp"
 #include "rmw_fastrtps_shared_cpp/custom_participant_info.hpp"
 #include "rmw_fastrtps_shared_cpp/custom_service_info.hpp"
 #include "rmw_fastrtps_shared_cpp/namespace_prefix.hpp"
+#include "rmw_fastrtps_shared_cpp/qos.hpp"
+#include "rmw_fastrtps_shared_cpp/rmw_common.hpp"
 
 #include "rmw_fastrtps_cpp/identifier.hpp"
 
 #include "./type_support_common.hpp"
-#include "./qos.hpp"
 
 using Domain = eprosima::fastrtps::Domain;
 using Participant = eprosima::fastrtps::Participant;
@@ -115,8 +115,8 @@ rmw_create_service(
   response_members = static_cast<const message_type_support_callbacks_t *>(
     service_members->response_members_->data);
 
-  std::string request_type_name = _create_type_name(request_members, "srv");
-  std::string response_type_name = _create_type_name(response_members, "srv");
+  std::string request_type_name = _create_type_name(request_members);
+  std::string response_type_name = _create_type_name(response_members);
 
   if (!Domain::getRegisteredType(participant, request_type_name.c_str(),
     reinterpret_cast<TopicDataType **>(&info->request_type_support_)))
